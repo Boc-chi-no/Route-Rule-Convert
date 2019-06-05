928ab476cf9f0eb0761654d08e5dd7e8759b86d6
ling <cnljea@outlook.com>
2019-06-05 10:38:40 +0800

    Add openvpn route and optimize

--- cidr_to_route.py
+++ cidr_to_route.py
@@ -2,6 +2,8 @@ import sys,getopt
 import socket
 import struct
 
+help = 'cidr_to_route.py -i <inputfile> -o <outputfile> -c <cmroute|openvpn>'
+
 def cidr_to_netmask(cidr):
     network, net_bits = cidr.split('/')
     host_bits = 32 - int(net_bits)
@@ -11,30 +13,39 @@ def cidr_to_netmask(cidr):
 def main(argv):
 	inputfile = ''
 	outputfile = ''
-	cmroute = ""
+	config = ""
+	out_route = ""
 	network = ""
 	try:
-		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+		opts, args = getopt.getopt(argv,"hi:o:c:",["ifile=","ofile=","config="])
 	except getopt.GetoptError:
-		print ('cidr_to_route.py -i <inputfile> -o <outputfile>')
+		print (help)
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt == '-h':
-			print ('cidr_to_route.py -i <inputfile> -o <outputfile>')
+			print (help)
 			sys.exit()
 		elif opt in ("-i", "--ifile"):
 			inputfile = arg
 		elif opt in ("-o", "--ofile"):
 			outputfile = arg
+		elif opt in ("-c", "--config"):
+			config = arg
+	if inputfile == "" or outputfile == "" or config == "":
+		print (help)
+		sys.exit(2)
 	file = open(inputfile)
 	for line in file:
 		try:
 			network=cidr_to_netmask(line)
-			cmroute += "add %s mask %s default METRIC default IF default\n" % (network)
+			if config == "openvpn" :
+				out_route += "route %s %s vpn_gateway\n" % (network)
+			elif config == "cmroute" :
+				cmroute += "add %s mask %s default METRIC default IF default\n" % (network)
 		except Exception:
 			pass
 	fo = open(outputfile, "w+")
-	fo.write(cmroute)
+	fo.write(out_route)
 	fo.close()
 	
 if __name__ == "__main__":
